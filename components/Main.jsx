77d1bae10e8998464eb88705c994e5864e7ca240
Bem <minhhieu21193@gmail.com>
2022-11-26 17:04:20 +0700

    first commit

--- components/Main.jsx
+++ components/Main.jsx
@@ -13,11 +13,11 @@ const Main = () => {
             LET&#39;S BUILD SOMETHING TOGETHER
           </p>
           <h1 className="py-4 text-gray-700">
-            Hi, I&#39;m <span className="text-[#5651e5]"> Hiếu</span>
+            Hi, I'm <span className="text-[#5651e5]"> Hiếu</span>
           </h1>
           <h1 className="py-2 text-gray-700">A Front-End Web Developer</h1>
           <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
-            I’m focused on building responsive front-end web applications
+            I am focused on building responsive front-end web applications
             integrating back-end technologies.
           </p>
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -127,7 +127,7 @@ const Navbar = () => {
             </div>
             <div className="border-b border-gray-300 my-4">
               <p className="w-[85%] md:w-[90%] py-4">
-                Let&#39;s build something legendary together
+                Let's build something legendary together
               </p>
             </div>
           </div>
@@ -166,7 +166,7 @@ const Navbar = () => {
             </ul>
             <div className="pt-40">
               <p className="uppercase tracking-widest text-[#5651e5]">
-                Let&#39;s Connect
+                Let's Connect
               </p>
               <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
                 <a
