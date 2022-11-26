1eeae2fb83ab494c7796ab5d98c8bd2423d345ff
Bem <minhhieu21193@gmail.com>
2022-11-26 17:09:33 +0700

    first commit

--- components/About.jsx
+++ components/About.jsx
@@ -14,13 +14,13 @@ const About = () => {
           <h2 className="py-4">Who I Am</h2>
           <p className="py-2 text-gray-600">
             I specialize in building web responsive front-end UI applications
-            that connect with API and other backend technologies. I am
-            passionate about learning new technologies and understand there is
-            more than one way to accomplish a task. Though I am most proficient
-            in building front-end applications using HTML, CSS, Javascript, and
-            React, I am a quick learner and can pick up new tech stacks as
-            needed. I believe that being a great developer is not using one
-            specific language, but choosing the best tool for the job.
+            that connect with API&rsquo;s and other backend technologies.
+            I&rsquo;m passionate about learning new technologies and understand
+            there is more than one way to accomplish a task. Though I am most
+            proficient in building front-end applications using HTML, CSS,
+            Javascript, and React, I am a quick learner and can pick up new tech
+            stacks as needed. I believe that being a great developer is not
+            using one specific language, but choosing the best tool for the job.
           </p>
 
           <Link href="/#projects">
